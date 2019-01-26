1e5ccd31869156aa3c1b6e9d1d36247ed57a322d
jcaspick <jake.caspick@gmail.com>
2019-01-26 12:18:12 -0300

    initial commit

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public List<Room> doors;
+    public List<Room> sharedWalls;
+    public bool isLit = false;
+}
